9e494a4c86d4852cb3a83c152d8fcc2b0efbd61a
Rodrigo Lopes <rodlps22@gmail.com>
2016-02-17 02:16:44 +0000

    add something

--- main.go
+++ main.go
@@ -2,37 +2,40 @@ package main
 
 import (
 	"flag"
-	"github.com/rodkranz/FindLinksWeb/interfaces"
-	"github.com/rodkranz/FindLinksWeb/engine"
 	"os"
+	"runtime"
+
+	"github.com/rodkranz/FindLinksWeb/engine"
+	"github.com/rodkranz/FindLinksWeb/interfaces"
 	"github.com/rodkranz/FindLinksWeb/searchers"
 )
 
-var config 			 = &interfaces.Configuration{}
-var SearchersDefault = []string{"google","yahoo"}
+var config = &interfaces.Configuration{}
+var SearchersDefault = []string{"google", "yahoo"}
 
 func init() {
-	flag.StringVar(&config.Text, 	    "t", 		 "any news", 	        "text that you looking for.")
-	flag.StringVar(&config.Output, 	    "out", 		 "output.log", 	        "Define that result will be save at file.")
-	flag.StringVar(&config.UserAgent,   "use-agent", "Go-http-client/1.2",  "Change the browser that you will you in your search.")
-	flag.IntVar(&config.Page, 		    "p", 		 1,				        "Pagination number.")
-	flag.Var(&config.Searchers, 	    "searchers", "Web Search")
+	flag.StringVar(&config.Text, "t", "any news", "text that you looking for.")
+	flag.StringVar(&config.Output, "out", "output.log", "Define that result will be save at file.")
+	flag.StringVar(&config.UserAgent, "use-agent", "Go-http-client/1.2", "Change the browser that you will you in your search.")
+	flag.IntVar(&config.Page, "p", 1, "Pagination number.")
+	flag.Var(&config.Searchers, "searchers", "Web Search")
 
 	flag.Parse()
 
 	if len(config.Searchers) == 0 {
-		config.Searchers = SearchersDefault;
+		config.Searchers = SearchersDefault
 	}
 }
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	yahoo   := searchers.NewYahoo()
-	bing    := searchers.NewBing()
-	google  := searchers.NewGoogle()
-	duck    := searchers.NewDuck()
+	yahoo := searchers.NewYahoo()
+	bing := searchers.NewBing()
+	google := searchers.NewGoogle()
+	duck := searchers.NewDuck()
 
-	engine 	:= engine.NewEngine(config)
+	engine := engine.NewEngine(config)
 
 	engine.AddEngine(bing, google, yahoo, duck)
 	engine.Run()
@@ -40,4 +43,3 @@ func main() {
 
 	os.Exit(0)
 }
-
